4b6a307f7fb1d4523151a49fc98049a15e9ed177
K-ash-ish <57871796+K-ash-ish@users.noreply.github.com>
2021-07-24 11:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 
-
-const panels = document.querySelectorAll(".panel");
-
-function toggleOpen(){
-    this.classList.toggle("open");
-    this.classList.toggle("open-active");
-}
-panels.forEach(function(input){
-    input.addEventListener("click", toggleOpen);
-});
